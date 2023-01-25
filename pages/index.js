aaeb86c2fcddec44609dc513795fe4608296b90b
faust6669 <123564743+faust6669@users.noreply.github.com>
2023-01-25 11:15:42 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+  </body> {
+       background-image: url(https://cdn1.epicgames.com/ue/product/Screenshot/01-1920x1080-e9b162d2ad3168704376ab55e1088891.jpg?resize=1&w=1920);</style>
+	   
+
+</body>
 
 export default function Home() {
   return (
