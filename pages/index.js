fd18913c78aedb955bf021c0212d2542498f283e
faust6669 <123564743+faust6669@users.noreply.github.com>
2023-01-25 11:12:06 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Kiss My Ass" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+                          </code>
         </p>
       </main>
 
